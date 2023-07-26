ff7eaf76a897078e8af826ac4cf8c6122b94b749
Kajaia <lashakajaia31@gmail.com>
2023-07-26 17:43:55 +0400

    Added link and subtitle

--- assets/scripts.js
+++ assets/scripts.js
@@ -16,7 +16,7 @@ function initMap() {
 
   let currentInfoWindow = null;
 
-  function addMarker({ lat, lng, letter, title, image }) {
+  function addMarker({ lat, lng, letter, title, subtitle, link, image }) {
     const marker = new google.maps.Marker({
       position: { lat, lng },
       map: map,
@@ -27,9 +27,13 @@ function initMap() {
     });
 
     const contentString = `
-    <div>
-      <img width="100%" height="150px" src="${image}" alt="${title}" style="border-radius: 6px">
-      <h3>${title}</h3>
+    <div class="center">
+      <img width="100%" height="130px" src="${image}" alt="${title}" style="border-radius: 6px">
+      <h3>
+        ${title}
+        <span class="subtitle">${subtitle}</span>
+      </h3>
+      ${link ? `<a class="link" href="${link}" target="_blank">ნახვა</a>` : ""}
     </div>
   `;
 
@@ -54,8 +58,10 @@ function initMap() {
       lat: 41.6562587,
       lng: 41.64067,
       title: "ა",
+      subtitle: "ნბანის კოშკი",
       text: "ა",
       letter: "a",
+      link: null,
       image:
         "https://anbanitour.ge/_cdn/2210/00/95/f518e60f7c71464db6c1758040537ee2/a.png",
     },
@@ -64,8 +70,10 @@ function initMap() {
       lat: 41.695768,
       lng: 41.712146,
       title: "ბ",
+      subtitle: "ოტანიკური ბაღი",
       text: "ბ",
       letter: "b",
+      link: "http://bbg.ge/ge/home",
       image:
         "https://anbanitour.ge/_cdn/2210/00/97/b33e2e324e4040a39fdcc4966889c6f1/b.png",
     },
@@ -74,8 +82,10 @@ function initMap() {
       lat: 41.57317,
       lng: 41.573216,
       title: "გ",
+      subtitle: "ონიოს ციხე",
       text: "გ",
       letter: "g",
+      link: "https://visitajara.com/sights/189",
       image:
         "https://anbanitour.ge/_cdn/2210/00/99/c9ce6aea060241b1bd84d0ce286ad528/g.png",
     },
@@ -84,8 +94,10 @@ function initMap() {
       lat: 41.6466546,
       lng: 42.1072594,
       title: "დ",
+      subtitle: "ანდალოს ხიდი",
       text: "დ",
       letter: "d",
+      link: "https://visitajara.com/sights/25",
       image:
         "https://anbanitour.ge/_cdn/2210/01/01/11e42b80ddbb449c911093c48002e2e1/d.png",
     },
@@ -94,8 +106,10 @@ function initMap() {
       lat: 41.65112,
       lng: 41.636191,
       title: "ე",
+      subtitle: "ვროპის მოედანი",
       text: "ე",
       letter: "e",
+      link: null,
       image:
         "https://anbanitour.ge/_cdn/2210/01/03/ee71cddbf94144138c0928f7f73b1cee/e.png",
     },
@@ -104,8 +118,10 @@ function initMap() {
       lat: 41.647591,
       lng: 41.620594,
       title: "ვ",
+      subtitle: "ელობილიკი",
       text: "ვ",
       letter: "v",
+      link: null,
       image:
         "https://anbanitour.ge/_cdn/2210/01/05/640cee69e42847818c52b6ebe5c05d46/v.png",
     },
@@ -114,8 +130,10 @@ function initMap() {
       lat: 41.65642,
       lng: 41.633144,
       title: "ზ",
+      subtitle: "ღვა",
       text: "ზ",
       letter: "z",
+      link: null,
       image:
         "https://anbanitour.ge/_cdn/2210/01/07/d58f14c03c8840a6b1d26f4df8522f0f/z.png",
     },
@@ -124,8 +142,10 @@ function initMap() {
       lat: 41.6268891,
       lng: 42.3125161,
       title: "თ",
+      subtitle: "აგო",
       text: "თ",
       letter: "t",
+      link: "https://visitajara.com/routes/8",
       image:
         "https://anbanitour.ge/_cdn/2210/01/09/ac01fca3423b4267ac2b327b8bc88b21/t.png",
     },
@@ -134,8 +154,10 @@ function initMap() {
       lat: 41.797924,
       lng: 41.786068,
       title: "ი",
+      subtitle: "ახნი",
       text: "ი",
       letter: "i",
+      link: null,
       image:
         "https://anbanitour.ge/_cdn/2210/01/11/eb8e29ab62974ddab494850138fc0aec/i.png",
     },
@@ -144,8 +166,10 @@ function initMap() {
       lat: 41.545749,
       lng: 41.561787,
       title: "კ",
+      subtitle: "ვარიათი",
       text: "კ",
       letter: "k",
+      link: null,
       image:
         "https://anbanitour.ge/_cdn/2210/01/13/392cccc2584248c5899aacb453b7c2c5/k.png",
     },
@@ -154,8 +178,10 @@ function initMap() {
       lat: 41.521867,
       lng: 41.549204,
       title: "ლ",
+      subtitle: "აზური მუზეუმი",
       text: "ლ",
       letter: "l",
+      link: null,
       image:
         "https://anbanitour.ge/_cdn/2210/01/63/68e7ce357ddb43dca6b9daa87066e759/artboard-7.png",
     },
@@ -164,8 +190,10 @@ function initMap() {
       lat: 41.48929,
       lng: 41.858906,
       title: "მ",
+      subtitle: "აჭახელა",
       text: "მ",
       letter: "m",
+      link: "https://infoajara.com/u/9886689",
       image:
         "https://anbanitour.ge/_cdn/2210/01/17/bda0c84290174e229cf12517464b00bd/m.png",
     },
@@ -174,8 +202,10 @@ function initMap() {
       lat: 41.661698,
       lng: 41.680265,
       title: "ნ",
+      subtitle: "ობელები",
       text: "ნ",
       letter: "n",
+      link: "https://visitajara.com/sights/6",
       image:
         "https://anbanitour.ge/_cdn/2210/01/61/19b97c7bec7047cbb87876db963918b5/n.png",
     },
@@ -184,8 +214,10 @@ function initMap() {
       lat: 41.8562561,
       lng: 41.9275329,
       title: "ო",
+      subtitle: "და",
       text: "ო",
       letter: "o",
+      link: "https://www.facebook.com/visitbatumi/posts/4410646149009465/",
       image:
         "https://anbanitour.ge/_cdn/2210/01/19/5285ef17297949af9d60b01f1ff3ad29/o.png",
     },
@@ -194,8 +226,10 @@ function initMap() {
       lat: 41.768576,
       lng: 41.753206,
       title: "პ",
+      subtitle: "ეტრა",
       text: "პ",
       letter: "p",
+      link: "https://visitajara.com/sights/45",
       image:
         "https://anbanitour.ge/_cdn/2210/01/21/8075f55610a44b1084da573f487ed83c/p.png",
     },
@@ -204,8 +238,10 @@ function initMap() {
       lat: 41.863688,
       lng: 41.789911,
       title: "ჟ",
+      subtitle: "ანგბადი",
       text: "ჟ",
       letter: "j",
+      link: null,
       image:
         "https://anbanitour.ge/_cdn/2210/01/23/c2ea5b1dd8d34ec69ff0c440b1c86efe/j.png",
     },
@@ -214,8 +250,10 @@ function initMap() {
       lat: 41.650014,
       lng: 41.625316,
       title: "რ",
+      subtitle: "ვაფეხა",
       text: "რ",
       letter: "r",
+      link: null,
       image:
         "https://anbanitour.ge/_cdn/2210/01/25/249393ab502c471bbb49d9af4bb60317/r.png",
     },
@@ -224,8 +262,10 @@ function initMap() {
       lat: 41.58457,
       lng: 42.329392,
       title: "ს",
+      subtitle: "ხალთა",
       text: "ს",
       letter: "s",
+      link: "https://infoajara.com/u/7291734",
       image:
         "https://anbanitour.ge/_cdn/2210/01/27/e9687119463b4acaa4442f3636890e14/s.png",
     },
@@ -234,8 +274,10 @@ function initMap() {
       lat: 41.770887,
       lng: 42.090285,
       title: "ტ",
+      subtitle: "ბიყელი",
       text: "ტ",
       letter: "t",
+      link: "https://visitajara.com/sights/188",
       image:
         "https://anbanitour.ge/_cdn/2210/01/29/da64825e25874b1c922ab23ae61b1147/t.png",
     },
@@ -244,8 +286,10 @@ function initMap() {
       lat: 41.636654,
       lng: 41.618648,
       title: "ღ",
+      subtitle: "ამის ბათუმი",
       text: "ღ",
       letter: "g",
+      link: null,
       image:
         "https://anbanitour.ge/_cdn/2210/01/37/e75b582d999c4f6993d20c315478bcf3/g.png",
     },
@@ -254,8 +298,10 @@ function initMap() {
       lat: 41.696954,
       lng: 42.310812,
       title: "უ",
+      subtitle: "ჩხო",
       text: "უ",
       letter: "u",
+      link: "https://visitajara.com/routes/16",
       image:
         "https://anbanitour.ge/_cdn/2210/01/31/9e811c48968d445584e26676c23ad1ce/u.png",
     },
@@ -264,8 +310,10 @@ function initMap() {
       lat: 41.68447,
       lng: 41.729928,
       title: "ფ",
+      subtitle: "რინველებზე დაკვირვება",
       text: "ფ",
       letter: "p",
+      link: "https://visitajara.com/routes/15",
       image:
         "https://anbanitour.ge/_cdn/2210/01/33/834d9fd888654eaab059c081b6674a81/p.png",
     },
@@ -274,8 +322,10 @@ function initMap() {
       lat: 41.8471496,
       lng: 41.7777128,
       title: "ქ",
+      subtitle: "ობულეთი",
       text: "ქ",
       letter: "q",
+      link: "https://tinyurl.com/52y96yza",
       image:
         "https://anbanitour.ge/_cdn/2210/01/35/63083b485457467fa1f1eabe8ce573f2/q.png",
     },
@@ -284,8 +334,10 @@ function initMap() {
       lat: 41.64938,
       lng: 41.64485,
       title: "ყ",
+      subtitle: "ავა ბათუმში",
       text: "ყ",
       letter: "k",
+      link: null,
       image:
         "https://anbanitour.ge/_cdn/2210/01/39/853f758d95f44a029f2e9965f9c1e26f/k.png",
     },
@@ -294,8 +346,10 @@ function initMap() {
       lat: 41.655928,
       lng: 41.6414262,
       title: "შ",
+      subtitle: "უქურა",
       text: "შ",
       letter: "sh",
+      link: null,
       image:
         "https://anbanitour.ge/_cdn/2210/01/45/107b09e0de414d2aa236975890fd1b5e/sh.png",
     },
@@ -304,8 +358,10 @@ function initMap() {
       lat: 41.759875,
       lng: 41.746332,
       title: "ც",
+      subtitle: "იხისძირი",
       text: "ც",
       letter: "ts",
+      link: "https://tinyurl.com/mvw2xh4j",
       image:
         "https://anbanitour.ge/_cdn/2210/01/47/e812a53a707b42838dd451fe52f9fbe4/ts.png",
     },
@@ -314,8 +370,10 @@ function initMap() {
       lat: 41.651531,
       lng: 41.638223,
       title: "ძ",
+      subtitle: "ველი ბათუმი",
       text: "ძ",
       letter: "dz",
+      link: null,
       image:
         "https://anbanitour.ge/_cdn/2210/01/49/95a498823b2140e6859fa4d73785ad69/dz.png",
     },
@@ -324,8 +382,10 @@ function initMap() {
       lat: 41.680043,
       lng: 41.860286,
       title: "წ",
+      subtitle: "ვიმა",
       text: "წ",
       letter: "ts",
+      link: "https://tinyurl.com/muc8vnx4",
       image:
         "https://anbanitour.ge/_cdn/2210/01/51/4353e3c8e2c94ab5886d90744a085ea4/ts.png",
     },
@@ -334,8 +394,10 @@ function initMap() {
       lat: 41.47672,
       lng: 42.469798,
       title: "ჩ",
+      subtitle: "ირუხი",
       text: "ჩ",
       letter: "ch",
+      link: "https://visitajara.com/alpine-ajara/2",
       image:
         "https://anbanitour.ge/_cdn/2210/01/43/5695dcf4f52f4aea854da2d90696523e/ch.png",
     },
@@ -344,8 +406,10 @@ function initMap() {
       lat: 41.705598,
       lng: 42.17965,
       title: "ჭ",
+      subtitle: "ჭვანა",
       text: "ჭ",
       letter: "ch",
+      link: "https://tinyurl.com/477fhd4t",
       image:
         "https://anbanitour.ge/_cdn/2210/01/53/53dd826e3a6f4d96b679289b0bed6244/ch.png",
     },
@@ -354,8 +418,10 @@ function initMap() {
       lat: 41.541672,
       lng: 42.565229,
       title: "ხ",
+      subtitle: "ხიხანი",
       text: "ხ",
       letter: "kh",
+      link: "https://visitajara.com/sights/111",
       image:
         "https://anbanitour.ge/_cdn/2210/01/55/64467cdebd9044b08713c0d6751ebb8a/kh.png",
     },
@@ -364,8 +430,10 @@ function initMap() {
       lat: 41.569776,
       lng: 41.979913,
       title: "ჯ",
+      subtitle: "ჯარა",
       text: "ჯ",
       letter: "j",
+      link: "https://infoajara.com/u/3845019",
       image:
         "https://anbanitour.ge/_cdn/2210/01/57/61c8e663faff48a197620752c3215720/jara.png",
     },
@@ -374,8 +442,10 @@ function initMap() {
       lat: 41.625699,
       lng: 41.656162,
       title: "ჰ",
+      subtitle: "ჰორიზონტი",
       text: "ჰ",
       letter: "h",
+      link: null,
       image:
         "https://anbanitour.ge/_cdn/2210/01/59/23ebaef11cfc41ca8cf2e9d2dcc45f57/h.png",
     },
